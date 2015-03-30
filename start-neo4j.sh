368b0bc7f3f95676b79362443a3a9db56318f321
Ben Bieker <mail@ben-bieker.de>
2015-03-30 10:17:27 +0200

    use neo4j 2.2.0 in travis and disable authentication

--- start-neo4j.sh
+++ start-neo4j.sh
@@ -1,9 +1,10 @@
 ﻿#!/bin/sh
 
-DIR="neo4j-community-2.1.6"
+DIR="neo4j-community-2.2.0"
 FILE="$DIR-unix.tar.gz"
 
 wget "http://dist.neo4j.org/$FILE"
 tar zxf $FILE
+sed -i "s/auth_enabled\=true/auth_enabled\=false/g" $DIR/conf/neo4j-server.properties
 $DIR/bin/neo4j start
 sleep 3
